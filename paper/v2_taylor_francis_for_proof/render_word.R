d6acfe50d66dcc4f731f88a67e27eb80e67b3f18
etranos <tranosm@yahoo.com>
2021-06-01 08:35:48 +0100

    figures for proof

---
+++ paper/v2_taylor_francis_for_proof/render_word.R
@@ -0,0 +1,5 @@
+library(rmarkdown)
+
+# render to .Rmd to word
+path <- "C:/Users/nw19521/OneDrive - University of Bristol/projects/broadband.speed.covid/paper/v2_taylor_francis_for_proof/broadband.speed.covid.Rmd"
+render(path, word_document())
--- paper/v2_taylor_francis_for_proof/render_word_bw_images.R
+++ paper/v2_taylor_francis_for_proof/render_word_bw_images.R
@@ -1,24 +1,35 @@
-library(rmarkdown)
 library(magick)
 
-# render to .Rmd to word
-path <- "C:/Users/nw19521/OneDrive - University of Bristol/projects/broadband.speed.covid/paper/v2_taylor_francis_for_proof/broadband.speed.covid.Rmd"
-render(path, word_document())
+# convert Figures to bw
+path.in <- "C:/Users/nw19521/OneDrive - University of Bristol/projects/broadband.speed.covid/paper/v2_taylor_francis_for_proof/figures"
+path.out <- "C:/Users/nw19521/OneDrive - University of Bristol/projects/broadband.speed.covid/paper/v2_taylor_francis_for_proof/figures_for_word_submission"
 
-# convert Fig 1 to bw
-path <- "C:/Users/nw19521/OneDrive - University of Bristol/projects/broadband.speed.covid/paper/v2_taylor_francis_for_proof/figures_for_word_submission"
-
-fig1.path <- paste0(path, "/Fig1.png")
-
-fig1 <- image_read(fig1.path)
+# Fig 1
+# fig1.path.in <- paste0(path.in, "/time.var.plot2020.png")
+fig1.path.in <- paste0(path.in, "/Time Var 2020.tiff")
+fig1 <- image_read(fig1.path.in)
 fig1 <- fig1 %>% image_quantize(colorspace = 'gray')
 
-image_write(fig1, path = fig1.path, format = "png")
+fig1.path.out <- paste0(path.out, "/Fig1.tiff")
 
-# convert Fig 2 and 2 to bw
-fig2.path <- paste0(path, "/Fig2.png")
+# opt. 1
+# fig1
+# tiff(fig1.path.out, res = 600)
+# dev.off()
 
-fig2 <- image_read(fig1.path)
+# opt. 2
+# fig1
+# ggsave(fig1.path.out, device='tiff', dpi=600)
+
+#opt. 3 -- it works
+image_write(fig1, path = fig1.path.out) #, format = "png")
+
+# Fig 2
+# fig2.path.in <- paste0(path.in, "/time.var.plot2019.png")
+fig2.path.in <- paste0(path.in, "/TimeVar2019.tiff")
+fig2 <- image_read(fig2.path.in)
 fig2 <- fig2 %>% image_quantize(colorspace = 'gray')
 
-image_write(fig2, path = fig2.path, format = "png")
+fig2.path.out <- paste0(path.out, "/Fig2.tiff")
+
+image_write(fig2, path = fig2.path.out, density = "600")
